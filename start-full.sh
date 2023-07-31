c6e940847df813324163391b1716f6ac2f90733f
Binge <zhiying8710@hotmail.com>
2023-07-31 12:17:10 +0800

    优化

--- start-full.sh
+++ start-full.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+export LD_LIBRARY_PATH=/usr/local/lib
 export COIN=Bitcoin
-export DB_DIRECTORY=/home/ubuntu/.electrumx/data
+export DB_DIRECTORY=/root/.electrumx/data
+export DB_ENGINE=rocksdb
 export DAEMON_URL=http://idclub:Bitcoin2088@127.0.0.1:2088
-export ELECTRUMX=/home/ubuntu/.electrumx/electrumx_server
+export ELECTRUMX=/root/.electrumx/electrumx_server
 export USERNAME=ubuntu
 export SERVICES=tcp://0.0.0.0:50001,rpc://0.0.0.0:8000,ws://0.0.0.0:50003
 export MAX_SESSIONS=5000
@@ -17,4 +18,4 @@ export INITIAL_CONCURRENT=10000
 export REQUEST_SLEEP=100
 export UNSYNC_MEMPOOL=0
 
-nohup python3 electrumx_server full > out-full.log 2>&1 &
+nohup python3 electrumx_server > out-full.log 2>&1 &
--- stop-full.sh
+++ stop-full.sh
@@ -4,7 +4,7 @@ python3 electrumx_rpc -p 8000 stop
 
 while :
 do
-        pid=`ps aux | grep -v grep | grep 'electrumx_server' | grep 'full' | awk '{print $2}'`
+        pid=`ps aux | grep -v grep | grep 'electrumx_server' | awk '{print $2}'`
         if [ "$pid"x != ""x ]; then
                 echo "electrumx_server is stopping!"
                 sleep 3s
